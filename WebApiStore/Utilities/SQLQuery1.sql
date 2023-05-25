c1f471fa1a012d0762909226625f5712d7869a1d
SamySalazar <samuel.salazarll@uanl.edu.mx>
2023-05-25 17:15:57 -0600

    Create SQLQuery1.sql

---
+++ WebApiStore/Utilities/SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿SELECT * FROM products;
+
+SELECT TOP(1) Category, UserId, SUM(Quantity) AS "Quantity"
+  FROM Products AS p
+	   INNER JOIN OrdersProducts AS op ON p.Id = op.ProductId
+	   INNER JOIN Orders AS o ON op.OrderId = o.Id
+ WHERE UserId = '45ab711a-971e-476d-a73e-4b58ba3865bb'
+ GROUP BY Category, UserId
+ ORDER BY Quantity DESC
